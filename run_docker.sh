0a3a5260e2698251796a01c3b9e8f81c7006fe8c
X-CCS <CCS695146667@163.com>
2021-04-07 06:58:29 +0000

    update

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@
 #Date：2021-02-01
 #Version：1.0
 #Description：The first script		
-echo "run 44ad293b8371 docker!"
+echo "run 407cc0c4235c docker!"
 #show hostname
-docker start 44ad293b8371;
-docker attach 44ad293b8371;
+docker start 407cc0c4235c;
+docker attach 407cc0c4235c;
